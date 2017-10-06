de5296747730428774016a91c1cfe82166f06a18
Lison <zhigang.li@tendcloud.com>
2017-10-06 19:13:42 +0800

    solve the bug of breadcrumbNav
    by editting util.js

--- src/views/util.js
+++ src/views/util.js
@@ -23,24 +23,8 @@ util.getPathObjByName = function (vm, name) {
 };
 
 util.setCurrentPath = function (vm, name) {
-    let currentPathObj = vm.$store.state.routers.filter((item) => {
-        if (item.children.length <= 1) {
-            return item.children[0].name === name;
-        } else {
-            let i = 0;
-            let childArr = item.children;
-            let len = childArr.length;
-            while (i < len) {
-                if (childArr[i].name === name) {
-                    return true;
-                }
-                i++;
-            }
-            return false;
-        }
-    })[0];
     let currentPathArr = [];
-    if (currentPathObj.children.length <= 1 && currentPathObj.name === 'home') {
+    if (name === 'home_index') {
         currentPathArr = [
             {
                 title: '首页',
@@ -48,7 +32,7 @@ util.setCurrentPath = function (vm, name) {
                 name: 'home_index'
             }
         ];
-    } else if (currentPathObj.children.length <= 1 && currentPathObj.name !== 'home') {
+    } else if (name === 'ownspace_index') {
         currentPathArr = [
             {
                 title: '首页',
@@ -56,32 +40,71 @@ util.setCurrentPath = function (vm, name) {
                 name: 'home_index'
             },
             {
-                title: currentPathObj.title,
+                title: '个人中心',
                 path: '',
-                name: name
+                name: 'ownspace_index'
             }
         ];
     } else {
-        let childObj = currentPathObj.children.filter((child) => {
-            return child.name === name;
-        })[0];
-        currentPathArr = [
-            {
-                title: '首页',
-                path: '/home',
-                name: 'home_index'
-            },
-            {
-                title: currentPathObj.title,
-                path: '',
-                name: currentPathObj.name
-            },
-            {
-                title: childObj.title,
-                path: currentPathObj.path + '/' + childObj.path,
-                name: name
+        let currentPathObj = vm.$store.state.routers.filter((item) => {
+            if (item.children.length <= 1) {
+                return item.children[0].name === name;
+            } else {
+                let i = 0;
+                let childArr = item.children;
+                let len = childArr.length;
+                while (i < len) {
+                    if (childArr[i].name === name) {
+                        return true;
+                    }
+                    i++;
+                }
+                return false;
             }
-        ];
+        })[0];
+        if (currentPathObj.children.length <= 1 && currentPathObj.name === 'home') {
+            currentPathArr = [
+                {
+                    title: '首页',
+                    path: '',
+                    name: 'home_index'
+                }
+            ];
+        } else if (currentPathObj.children.length <= 1 && currentPathObj.name !== 'home') {
+            currentPathArr = [
+                {
+                    title: '首页',
+                    path: '/home',
+                    name: 'home_index'
+                },
+                {
+                    title: currentPathObj.title,
+                    path: '',
+                    name: name
+                }
+            ];
+        } else {
+            let childObj = currentPathObj.children.filter((child) => {
+                return child.name === name;
+            })[0];
+            currentPathArr = [
+                {
+                    title: '首页',
+                    path: '/home',
+                    name: 'home_index'
+                },
+                {
+                    title: currentPathObj.title,
+                    path: '',
+                    name: currentPathObj.name
+                },
+                {
+                    title: childObj.title,
+                    path: currentPathObj.path + '/' + childObj.path,
+                    name: name
+                }
+            ];
+        }
     }
     vm.$store.commit('setCurrentPath', currentPathArr);
 
