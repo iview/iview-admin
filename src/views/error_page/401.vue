2bdfb50383370976aaeff0409ac928deb7e771b2
Lison <zhigang.li@tendcloud.com>
2017-10-12 09:59:05 +0800

    add 401 error page

--- src/views/error_page/401.vue
+++ src/views/error_page/401.vue
@@ -1,9 +1,35 @@
+<style lang="less">
+    @import './401.less';
+</style>
+
 <template>
-    <div>401</div>
+    <div class="error401">
+        <div class="error401-body-con">
+            <Card>
+                <div class="error401-body-con-title">
+                    4<span class="error401-0-span"><Icon type="android-lock"></Icon></span><span class="error401-key-span"><Icon type="key"></Icon></span>
+                </div>
+                <p class="error401-body-con-message">You don't have permission</p>
+                <div class="error401-btn-con">
+                    <Button @click="goHome" size="large" style="width: 200px;margin: 20px;" type="text">返回首页</Button>
+                    <Button @click="backPage" size="large" style="width: 200px;margin: 20px;" type="primary">返回上一页</Button>
+                </div>
+            </Card>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    //
+    methods: {
+        backPage () {
+            this.$router.go(-1);
+        },
+        goHome () {
+            this.$router.push({
+                name: 'home_index'
+            });
+        }
+    }
 };
 </script>
