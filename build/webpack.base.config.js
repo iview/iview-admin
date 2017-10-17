215fa009085ed6b084537b7477ad7fc71cc48b0b
Lison <zhigang.li@tendcloud.com>
2017-10-17 13:35:15 +0800

    update

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -72,7 +72,7 @@ module.exports = {
         extensions: ['.js', '.vue'],
         alias: {
             'vue': 'vue/dist/vue.esm.js',
-            '@': resolve('src')
+            '@': resolve('../src')
         }
     }
 };
