094869a146fffd8e0844e692079a4fbce20503cb
Lison <zhigang.li@tendcloud.com>
2017-10-17 10:09:35 +0800

    change avator path

--- src/views/login.vue
+++ src/views/login.vue
@@ -62,7 +62,7 @@ export default {
                 if (valid) {
                     Cookies.set('user', this.form.userName);
                     Cookies.set('password', this.form.password);
-                    this.$store.commit('setAvator', 'https://i.loli.net/2017/08/21/599a521472424.jpg');
+                    this.$store.commit('setAvator', 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3448484253,3685836170&fm=27&gp=0.jpg');
                     if (this.form.userName === 'iview_admin') {
                         Cookies.set('access', 0);
                     } else {
