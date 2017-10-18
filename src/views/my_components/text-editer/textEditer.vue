a4a9bb4629c3dcc82f7e87483b08fc099e790801
Lison <zhigang.li@tendcloud.com>
2017-10-18 10:43:50 +0800

    add loading spin for texteditor when plugins are loading

--- src/views/my_components/text-editer/textEditer.vue
+++ src/views/my_components/text-editer/textEditer.vue
@@ -1,16 +1,30 @@
+<style lang="less">
+    @import '../../../styles/loading.less';
+</style>
+
 <template>
     <div>
         <Card shadow>
-        <textarea class='tinymce-textarea' id="tinymceEditer"></textarea>
+            <textarea class='tinymce-textarea' id="tinymceEditer"></textarea>
         </Card>
+        <Spin fix v-if="spinShow">
+            <Icon type="load-c" size=18 class="demo-spin-icon-load"></Icon>
+            <div>加载组件中...</div>
+        </Spin>
     </div>
 </template>
 
 <script>
 import tinymce from 'tinymce';
 export default {
+    data () {
+        return {
+            spinShow: true
+        };
+    },
     mounted () {
         this.$nextTick(() => {
+            let vm = this;
             var height = document.body.offsetHeight - 300;
             tinymce.init({
                 selector: '#tinymceEditer',
@@ -19,19 +33,23 @@ export default {
                 height: height,
                 language: 'zh_CN.GB2312',
                 menubar: 'edit insert view format table tools',
-                theme: 'modern',
                 plugins: [
                     'advlist autolink lists link image charmap print preview hr anchor pagebreak imagetools',
-                    'searchreplace visualblocks visualchars code fullscreen fullpage',
+                    'searchreplace visualblocks visualchars code fullpage',
                     'insertdatetime media nonbreaking save table contextmenu directionality',
                     'emoticons paste textcolor colorpicker textpattern imagetools codesample'
                 ],
-                toolbar1: ' newnote print fullscreen preview | undo redo | insert | styleselect | forecolor backcolor bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image emoticons media codesample',
+                toolbar1: ' newnote print preview | undo redo | insert | styleselect | forecolor backcolor bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image emoticons media codesample',
                 autosave_interval: '20s',
                 image_advtab: true,
                 table_default_styles: {
                     width: '100%',
                     borderCollapse: 'collapse'
+                },
+                setup: function (editor) {
+                    editor.on('init', function (e) {
+                        vm.spinShow = false;
+                    });
                 }
             });
         });
