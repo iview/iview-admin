99dc440865530d05b402ed69a2393459db3b4273
Lison <zhigang.li@tendcloud.com>
2017-10-18 15:12:07 +0800

    update to v1.0.1

--- dist/themes/mobile/index.js
+++
@@ -1,7 +0,0 @@
-// Exports the "mobile" theme for usage with module loaders
-// Usage:
-//   CommonJS:
-//     require('tinymce/themes/mobile')
-//   ES2015:
-//     import 'tinymce/themes/mobile'
-require('./theme.js');
