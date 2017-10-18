1fecd8d80680dde806c0aa3c631abbef7696fffa
Lison <zhigang.li@tendcloud.com>
2017-10-18 11:14:00 +0800

    update

--- src/views/my_components/text-editer/textEditer.vue
+++ src/views/my_components/text-editer/textEditer.vue
@@ -33,6 +33,7 @@ export default {
                 height: height,
                 language: 'zh_CN.GB2312',
                 menubar: 'edit insert view format table tools',
+                theme_url: '../../../../dist/themes/modern/theme.min.js',
                 plugins: [
                     'advlist autolink lists link image charmap print preview hr anchor pagebreak imagetools',
                     'searchreplace visualblocks visualchars code fullpage',
