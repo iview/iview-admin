bc0cd494fd575c7843ed80e4fdc7c6f345f25c0e
zhigang.li <zhigang.li@tendcloud.com>
2017-11-22 10:51:51 +0800

    rewrite dragable table

--- src/views/tables/components/dragableTable.vue
+++ src/views/tables/components/dragableTable.vue
@@ -3,7 +3,7 @@
         <Table 
             :ref="refs" 
             :columns="columnsList" 
-            :data="tableData" 
+            :data="value" 
             highlight-row 
             border
         ></Table>
@@ -18,21 +18,33 @@ export default {
     props: {
         refs: String,
         columnsList: Array,
-        tableData: Array,
-        start: Function,
-        end: Function,
-        choose: Function
+        value: Array
     },
     methods: {
-        //
+        startFunc (e) {
+            this.$emit('on-start', e.oldIndex);
+        },
+        endFunc (e) {
+            let movedRow = this.value[e.oldIndex];
+            this.value.splice(e.oldIndex, 1);
+            this.value.splice(e.newIndex, 0, movedRow);
+            this.$emit('on-end', {
+                value: this.value,
+                from: e.oldIndex,
+                to: e.newIndex
+            });
+        },
+        chooseFunc (e) {
+            this.$emit('on-choose', e.oldIndex);
+        }
     },
     mounted () {
         var el = this.$refs[this.refs].$children[1].$el.children[1];
         let vm = this;
         Sortable.create(el, {
-            onStart: vm.start,
-            onEnd: vm.end,
-            onChoose: vm.choose
+            onStart: vm.startFunc,
+            onEnd: vm.endFunc,
+            onChoose: vm.chooseFunc
         });
     }
 };
