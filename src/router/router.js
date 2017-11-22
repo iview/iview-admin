a3990eb25828706242e7c1b2d10228df7d781038
zhigang.li <zhigang.li@tendcloud.com>
2017-11-22 16:07:51 +0800

    update

--- src/router/router.js
+++ src/router/router.js
@@ -152,6 +152,13 @@ export const appRouter = [
                 title: '数字渐变',
                 component: resolve => { require(['@/views/my-components/count-to/count-to.vue'], resolve); }
             }
+            // {
+            //     path: 'clipboard-page',
+            //     icon: 'clipboard',
+            //     name: 'clipboard-page',
+            //     title: '一键复制',
+            //     component: resolve => { require(['@/views/my-components/clipboard/clipboard.vue'], resolve); }
+            // }
         ]
     },
     {
