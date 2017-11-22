3e04770c4cfe75d607c4c3cf78a71c9c9fa4192a
zhigang.li <zhigang.li@tendcloud.com>
2017-11-22 12:01:57 +0800

    update

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -16,7 +16,7 @@ fs.open('./build/env.js', 'w', function(err, fd) {
 
 module.exports = merge(webpackBaseConfig, {
     output: {
-        publicPath: 'https://iview.github.io/iview-admin/dist/',
+        publicPath: 'https://iview.github.io/iview-admin/dist/',  // 修改 https://iv...admin 这部分为你的服务器域名 
         filename: '[name].[hash].js',
         chunkFilename: '[name].[hash].chunk.js'
     },
