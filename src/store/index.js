fbf01b58fff53ab7ef7e1bb23252b17e2108dcde
zhigang.li <zhigang.li@tendcloud.com>
2017-11-23 10:49:05 +0800

    fixed the bug that the opened pages will be cleared when you refresh browser

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import Util from '@/libs/util';
 
 import app from './modules/app';
 import user from './modules/user';
@@ -9,16 +8,10 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
     state: {
-        dontCache: ['text-editor', 'artical-publish']  // 在这里定义你不想要缓存的页面的name属性值(参见路由配置router.js)
+        //
     },
     mutations: {
-        increateTag (state, tagObj) {
-            if (!Util.oneOf(tagObj.name, state.dontCache)) {
-                state.app.cachePage.push(tagObj.name);
-                localStorage.cachePage = JSON.stringify(state.app.cachePage);
-            }
-            state.app.pageOpenedList.push(tagObj);
-        }
+        //
     },
     actions: {
 
--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -30,7 +30,8 @@ const app = {
             ...appRouter
         ],
         tagsList: [...otherRouter.children],
-        messageCount: 0
+        messageCount: 0,
+        dontCache: ['text-editor', 'artical-publish']  // 在这里定义你不想要缓存的页面的name属性值(参见路由配置router.js)
     },
     mutations: {
         setTagsList (state, list) {
@@ -177,6 +178,14 @@ const app = {
         },
         setMessageCount (state, count) {
             state.messageCount = count;
+        },
+        increateTag (state, tagObj) {
+            if (!Util.oneOf(tagObj.name, state.dontCache)) {
+                state.cachePage.push(tagObj.name);
+                localStorage.cachePage = JSON.stringify(state.cachePage);
+            }
+            state.pageOpenedList.push(tagObj);
+            localStorage.pageOpenedList = JSON.stringify(state.pageOpenedList);
         }
     }
 };
