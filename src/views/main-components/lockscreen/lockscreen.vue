0492434e69bad96350f7ffeacda189dfc28518cd
zhigang.li <zhigang.li@tendcloud.com>
2017-11-28 19:50:42 +0800

    fixed the bug that there always creat a new div element when unlock screen.

--- src/views/main-components/lockscreen/lockscreen.vue
+++ src/views/main-components/lockscreen/lockscreen.vue
@@ -34,26 +34,28 @@ export default {
         }
     },
     mounted () {
-        let lockdiv = document.createElement('div');
-        lockdiv.setAttribute('id', 'lock_screen_back');
-        lockdiv.setAttribute('class', 'lock-screen-back');
-        document.body.appendChild(lockdiv);
-        let lockScreenBack = document.getElementById('lock_screen_back');
-        let x = document.body.clientWidth;
-        let y = document.body.clientHeight;
-        let r = Math.sqrt(x * x + y * y);
-        let size = parseInt(r);
-        this.lockScreenSize = size;
-        window.addEventListener('resize', () => {
+        if (!document.getElementById('lock_screen_back')) {
+            let lockdiv = document.createElement('div');
+            lockdiv.setAttribute('id', 'lock_screen_back');
+            lockdiv.setAttribute('class', 'lock-screen-back');
+            document.body.appendChild(lockdiv);
+            let lockScreenBack = document.getElementById('lock_screen_back');
             let x = document.body.clientWidth;
             let y = document.body.clientHeight;
             let r = Math.sqrt(x * x + y * y);
             let size = parseInt(r);
             this.lockScreenSize = size;
-            lockScreenBack.style.transition = 'all 0s';
+            window.addEventListener('resize', () => {
+                let x = document.body.clientWidth;
+                let y = document.body.clientHeight;
+                let r = Math.sqrt(x * x + y * y);
+                let size = parseInt(r);
+                this.lockScreenSize = size;
+                lockScreenBack.style.transition = 'all 0s';
+                lockScreenBack.style.width = lockScreenBack.style.height = size + 'px';
+            });
             lockScreenBack.style.width = lockScreenBack.style.height = size + 'px';
-        });
-        lockScreenBack.style.width = lockScreenBack.style.height = size + 'px';
+        }
     }
 };
 </script>
