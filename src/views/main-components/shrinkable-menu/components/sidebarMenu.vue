2870d1f04c87555cbf936a8924c8d46d66accc55
zhigang.li <zhigang.li@tendcloud.com>
2017-12-26 13:05:47 +0800

    update after vue updated 2.5.13

--- src/views/main-components/shrinkable-menu/components/sidebarMenu.vue
+++ src/views/main-components/shrinkable-menu/components/sidebarMenu.vue
@@ -5,9 +5,9 @@
 <template>
     <Menu ref="sideMenu" :active-name="$route.name" :open-names="openNames" :theme="menuTheme" width="auto" @on-select="changeMenu">
         <template v-for="item in menuList">
-            <MenuItem v-if="item.children.length<=1" :name="item.children[0].name" :key="item.name">
-                <Icon :type="item.icon" :size="iconSize" :key="item.name"></Icon>
-                <span class="layout-text" :key="item.name">{{ itemTitle(item) }}</span>
+            <MenuItem v-if="item.children.length<=1" :name="item.children[0].name" :key="'menuitem' + item.name">
+                <Icon :type="item.icon" :size="iconSize" :key="'menuicon' + item.name"></Icon>
+                <span class="layout-text" :key="'title' + item.name">{{ itemTitle(item) }}</span>
             </MenuItem>
 
             <Submenu v-if="item.children.length > 1" :name="item.name" :key="item.name">
@@ -16,9 +16,9 @@
                     <span class="layout-text">{{ itemTitle(item) }}</span>
                 </template>
                 <template v-for="child in item.children">
-                    <MenuItem :name="child.name" :key="child.name">
-                        <Icon :type="child.icon" :size="iconSize" :key="child.name"></Icon>
-                        <span class="layout-text" :key="child.name">{{ itemTitle(child) }}</span>
+                    <MenuItem :name="child.name" :key="'menuitem' + child.name">
+                        <Icon :type="child.icon" :size="iconSize" :key="'icon' + child.name"></Icon>
+                        <span class="layout-text" :key="'title' + child.name">{{ itemTitle(child) }}</span>
                     </MenuItem>
                 </template>
             </Submenu>
