f6d9b123f939d7411996222e24fa53166923d233
zhigang.li <zhigang.li@tendcloud.com>
2018-01-09 10:20:44 +0800

    update

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import App from './app.vue';
 import '@/locale';
 import 'iview/dist/styles/iview.css';
 import VueI18n from 'vue-i18n';
+import util from './libs/util';
 
 Vue.use(VueI18n);
 Vue.use(iView);
@@ -27,7 +28,7 @@ new Vue({
         // 权限菜单过滤相关
         this.$store.commit('updateMenulist');
         // iview-admin检查更新
-        // util.checkUpdate(this);
+        util.checkUpdate(this);
     },
     created () {
         let tagsList = [];
