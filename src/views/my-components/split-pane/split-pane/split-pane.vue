d3b8abc40c7a38c7adbe6605b686b057ee88dd67
zhigang.li <zhigang.li@tendcloud.com>
2018-01-17 15:47:13 +0800

    update split-pane component

--- src/views/my-components/split-pane/split-pane/split-pane.vue
+++ src/views/my-components/split-pane/split-pane/split-pane.vue
@@ -17,7 +17,7 @@
                 <div 
                     :class="`${prefix}-trigger`" 
                     ref="trigger" 
-                    :style="{left: `${triggerOffset}%`}"
+                    :style="{left: triggerLeft}"
                     @mousedown="handleMousedown"
                     unselectable="on">
                 </div>
@@ -35,7 +35,7 @@
                 <div 
                     :class="`${prefix}-trigger`" 
                     ref="trigger"
-                    :style="{top: `${triggerOffset}%`}"
+                    :style="{top: triggerLeft}"
                     @mousedown="handleMousedown"
                     unselectable="on">
                 </div>
@@ -49,111 +49,133 @@
 
 <script>
 const oneOf = function (ele, targetArr) {
-    if (targetArr.indexOf(ele) >= 0) {
-        return true;
-    } else {
-        return false;
-    }
+	if (targetArr.indexOf(ele) >= 0) {
+		return true;
+	} else {
+		return false;
+	}
 };
 export default {
-    name: 'splitPane',
-    props: {
-        value: {
-            type: [Number, String],
-            default: 50
-        },
-        direction: {
-            type: String,
-            default: 'horizontal',
-            validator (val) {
-                return oneOf(val, ['vertical', 'horizontal']);
-            }
-        },
-        min: {
-            type: [Number, String],
-            default: 3
-        },
-        max: {
-            type: [Number, String],
-            default: 97
-        }
-    },
-    data () {
-        return {
-            prefix: 'split-pane',
-            canMove: false,
-            triggerOffset: 50,
-            triggerOldOffset: 50,
-            offset: {},
-            atMin: false,
-            atMax: false
-        };
-    },
-    computed: {
-        wraperClasses () {
-            return [
-                this.prefix,
-                this.direction === 'vertical' ? `${this.prefix}-vertical` : `${this.prefix}-horizontal`
-            ];
-        },
-        leftSize () {
-            return `${this.triggerOffset}%`;
-        },
-        rightSize () {
-            return `${100 - this.triggerOffset}%`;
-        },
-        minTransed () {
-            return this.transValue(this.min);
-        },
-        maxTransed () {
-            return this.transValue(this.max);
-        }
-    },
-    methods: {
-        handleMouseup () {
-            this.canMove = false;
-        },
-        transValue (val) {
-            return (typeof val === 'number') ? val : Math.floor(((parseFloat(val) / this.$refs.wraper.offsetWidth) * 10000)) / 100;
-        },
-        handleMousedown (e) {
-            this.canMove = true;
-            this.triggerOldOffset = this.triggerOffset;
-            this.offset = {
-                x: e.pageX,
-                y: e.pageY
-            };
-            e.preventDefault();
-        },
-        handleMouseout () {
-            this.canMove = false;
-        },
-        handleMousemove (e) {
-            if (this.canMove) {
-                let offset;
-                if (this.direction === 'horizontal') {
-                    offset = this.triggerOldOffset + Math.floor(((e.clientX - this.offset.x) / this.$refs.wraper.offsetWidth) * 10000) / 100;
-                } else {
-                    offset = this.triggerOldOffset + Math.floor(((e.clientY - this.offset.y) / this.$refs.wraper.offsetHeight) * 10000) / 100;
-                }
-                if (offset <= this.minTransed) {
-                    this.triggerOffset = Math.max(offset, this.minTransed);
-                } else {
-                    this.triggerOffset = Math.min(offset, this.maxTransed);
-                }
-                this.atMin = this.triggerOffset === this.minTransed;
-                this.atMax = this.triggerOffset === this.maxTransed;
-                e.atMin = this.atMin;
-                e.atMax = this.atMax;
-                this.$emit('input', offset);
-                this.$emit('on-trigger-moving', e);
-            }
-        }
-    },
-    mounted () {
-        if (this.value !== undefined) {
-            this.triggerOffset = (typeof this.value === 'number') ? this.value : Math.floor(((parseFloat(this.value) / this.$refs.wraper.offsetWidth) * 10000)) / 100;
-        }
-    }
+	name: 'splitPane',
+	props: {
+		value: {
+			type: [Number, String],
+			default: 50
+		},
+		direction: {
+			type: String,
+			default: 'horizontal',
+			validator (val) {
+				return oneOf(val, ['vertical', 'horizontal']);
+			}
+		},
+		min: {
+			type: [Number, String],
+			default: 3
+		},
+		max: {
+			type: [Number, String],
+			default: 97
+		},
+		right: {
+			type: Boolean,
+			default: false
+		}
+	},
+	data () {
+		return {
+			prefix: 'split-pane',
+			canMove: false,
+			triggerOffset: 50,
+			triggerOldOffset: 50,
+			offset: {},
+			atMin: false,
+			atMax: false
+		};
+	},
+	computed: {
+		wraperClasses () {
+			return [
+				this.prefix,
+				this.direction === 'vertical' ? `${this.prefix}-vertical` : `${this.prefix}-horizontal`
+			];
+		},
+		leftSize () {
+			return this.right ? `${100 - this.triggerOffset}%` : `${this.triggerOffset}%`;
+		},
+		rightSize () {
+			return this.right ? `${this.triggerOffset}%` : `${100 - this.triggerOffset}%`;
+		},
+		triggerLeft () {
+			return this.right ? `${100 - this.triggerOffset}%` : `${this.triggerOffset}%`;
+		},
+		minTransed () {
+			return this.transValue(this.min);
+		},
+		maxTransed () {
+			return this.transValue(this.max);
+		}
+	},
+	methods: {
+		handleMouseup (e) {
+			this.canMove = false;
+			this.$emit('on-moving-end', e);
+		},
+		transValue (val) {
+			return (typeof val === 'number') ? val : Math.floor(((parseFloat(val) / this.$refs.wraper.offsetWidth) * 10000)) / 100;
+		},
+		handleMousedown (e) {
+			this.canMove = true;
+			this.triggerOldOffset = this.triggerOffset;
+			this.offset = {
+				x: e.pageX,
+				y: e.pageY
+			};
+			this.$emit('on-moving-start', e);
+			e.preventDefault();
+		},
+		handleMouseout (e) {
+			this.canMove = false;
+			this.$emit('on-moving-end', e);
+		},
+		handleMousemove (e) {
+			if (this.canMove) {
+				let offset;
+				if (this.direction === 'horizontal') {
+					let moveSize = Math.floor(((e.clientX - this.offset.x) / this.$refs.wraper.offsetWidth) * 10000) / 100;
+					offset = this.triggerOldOffset + (this.right ? -moveSize : moveSize);
+				} else {
+					let moveSize = Math.floor(((e.clientY - this.offset.y) / this.$refs.wraper.offsetHeight) * 10000) / 100;
+					offset = this.triggerOldOffset + (this.right ? -moveSize : moveSize);
+				}
+				if (offset <= this.minTransed) {
+					this.triggerOffset = Math.max(offset, this.minTransed);
+				} else {
+					this.triggerOffset = Math.min(offset, this.maxTransed);
+				}
+				this.atMin = this.triggerOffset === this.minTransed;
+				this.atMax = this.triggerOffset === this.maxTransed;
+				e.atMin = this.atMin;
+				e.atMax = this.atMax;
+				this.$emit('input', offset);
+				this.$emit('on-trigger-moving', e);
+			}
+		}
+	},
+	watch: {
+		value (val) {
+			this.$nextTick(() => {
+				this.triggerOffset = (typeof val === 'number') ? val : Math.floor(((parseInt(val) / this.$refs.wraper.offsetWidth) * 10000)) / 100;
+			});
+		}
+	},
+	mounted () {
+		if (this.value !== undefined) {
+			this.$nextTick(() => {
+				this.triggerOffset = (typeof this.value === 'number') ? this.value : Math.floor(((parseInt(this.value) / this.$refs.wraper.offsetWidth) * 10000)) / 100;
+			});
+			this.triggerOffset = (typeof this.value === 'number') ? this.value : Math.floor(((parseInt(this.value) / this.$refs.wraper.offsetWidth) * 10000)) / 100;
+		}
+	}
 };
 </script>
