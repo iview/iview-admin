6a571e54dbf4326260b0bdcc7d02a08b9add22ab
YanHao <yanhao@imdada.cn>
2018-02-24 17:41:08 +0800

    chore: config update

--- build/config.js
+++ build/config.js
@@ -1,6 +1,13 @@
 import Env from './env';
 
 let config = {
-    env: Env
+    env: Env,
+    domain: Env === 'development'
+        ? 'http://127.0.0.1:8888'   // 开发环境url
+        : Env === 'production'      
+            ? 'https://www.url.com' // 生产环境url
+            : 'https://debug.url.com', // debug url
+    cdn: 'https://fe-source.cdn.com',   // 前端资源托管cdn
 };
+
 export default config;
--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -18,7 +18,8 @@ fs.open('./build/env.js', 'w', function(err, fd) {
 
 module.exports = merge(webpackBaseConfig, {
     output: {
-        publicPath: 'https://iview.github.io/iview-admin/dist/',  // 修改 https://iv...admin 这部分为你的服务器域名 
+        path: path.resolve(__dirname, '../dist', package.version),
+        publicPath: '/',
         filename: '[name].[hash].js',
         chunkFilename: '[name].[hash].chunk.js'
     },
