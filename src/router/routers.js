13c0e96549d7bdd423f61bf9c8b38622918b6790
zhigang.li <zhigang.li@tendcloud.com>
2018-04-23 19:06:59 +0800

    commit

--- src/router/routers.js
+++ src/router/routers.js
@@ -1,4 +1,31 @@
-let singlePage = [
-  //
+import Main from '@/view/main'
+
+const addChildPage = (name, path = '') => {
+  return {
+    path: name,
+    name: name,
+    component: () => import(`@/view/${path}${name}`)
+  }
+}
+
+export default [
+  {
+    path: '/',
+    name: 'pageNotShowInMenu',
+    component: Main,
+    children: [
+      addChildPage('home'), // 首页
+      addChildPage('ownspace', 'single-page/'), // 个人中心
+      addChildPage('message', 'single-page/'), // 消息中心
+      addChildPage('shopping', 'single-page/') // 购物中心
+    ]
+  },
+  {
+    path: '/international',
+    name: 'international',
+    component: Main,
+    children: [
+      addChildPage('index', )
+    ]
+  }
 ]
-export default singlePage
