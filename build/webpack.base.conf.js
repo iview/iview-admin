1cda264d650a394a31aa36c9229849af01035517
zhigang.li <zhigang.li@tendcloud.com>
2018-04-23 15:35:59 +0800

    update webpack alias

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,6 +36,8 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      '_c': resolve('src/components'),
+      '_conf': resolve('config')
     }
   },
   module: {
