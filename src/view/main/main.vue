4d1efb6aace7f7ecf86439b39fc71308f551c0d9
zhigang.li <zhigang.li@tendcloud.com>
2018-04-25 18:54:25 +0800

    update sider width

--- src/view/main/main.vue
+++ src/view/main/main.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout style="height: 100%" class="main">
-    <Sider collapsible :width="260" :collapsed-width="64" v-model="collapsed">
+    <Sider collapsible :width="210" :collapsed-width="64" v-model="collapsed">
       <side-menu :collapsed="collapsed" @on-select="turnToPage" :use-i18n="useI18n" :menu-list="menuList">
         <div class="logo-con">
           <img v-show="!collapsed"  :src="maxLogo" key="max-logo" />
