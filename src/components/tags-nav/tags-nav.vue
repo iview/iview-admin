06b98170ab06d4165dd25649005cf1f8d726ebe8
zhigang.li <zhigang.li@tendcloud.com>
2018-05-16 13:23:26 +0800

    标签导航增加on-close事件，返回关闭后的列表

--- src/components/tags-nav/tags-nav.vue
+++ src/components/tags-nav/tags-nav.vue
@@ -81,7 +81,15 @@ export default {
       }
     },
     handleTagsOption (type) {
-      console.log(type)
+      if (type === 'close-all') {
+        // 关闭所有，除了home
+        let res = this.list.filter(item => item.name === 'home')
+        this.$emit('on-close', res)
+      } else {
+        // 关闭除当前页和home页的其他页
+        let res = this.list.filter(item => item.name === this.value || item.name === 'home')
+        this.$emit('on-close', res)
+      }
     },
     handleClose (name) {
       this.$emit('on-close', name)
