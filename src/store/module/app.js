e35615f28fade06a8ab83c5580ab4a752c42b4cb
zhigang.li <zhigang.li@tendcloud.com>
2018-05-16 19:40:26 +0800

    修改setTagNavList

--- src/store/module/app.js
+++ src/store/module/app.js
@@ -9,14 +9,10 @@ export default {
     setBreadCrumb (state, routeMetched) {
       state.breadCrumbList = getBreadCrumbList(routeMetched)
     },
-    setTagNavList (state, newRoute) {
-      if (newRoute) {
-        const { name, path, meta } = newRoute
-        let newList = [...state.tagNavList]
-        if (newList.findIndex(item => item.name === name) >= 0) return newList
-        else newList.push({ name, path, meta })
-        state.tagNavList = [...newList]
-        setTagNavListInLocalstorage([...newList])
+    setTagNavList (state, list) {
+      if (list) {
+        state.tagNavList = [...list]
+        setTagNavListInLocalstorage([...list])
       } else state.tagNavList = getTagNavListFromLocalstorage()
     },
     addTag (state, item, type = 'unshift') {
--- src/view/main/main.vue
+++ src/view/main/main.vue
@@ -206,6 +206,13 @@ export default {
     handleCollapsedChange (state) {
       this.collapsed = state
     },
+    getNewTagList (newRoute) {
+      const { name, path, meta } = newRoute
+      let newList = [...this.tagNavList]
+      if (newList.findIndex(item => item.name === name) >= 0) return newList
+      else newList.push({ name, path, meta })
+      return newList
+    },
     handleClick (item) {
       this.$router.push({
         name: item.name
@@ -215,7 +222,7 @@ export default {
   watch: {
     '$route' (newRoute) {
       this.setBreadCrumb(newRoute.matched)
-      this.setTagNavList(newRoute)
+      this.setTagNavList(this.getNewTagList(newRoute))
       this.currentRoute = newRoute
     }
   },
