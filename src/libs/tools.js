16f6ded4b0e4eb22453a9f566eb20332e519b990
zhigang.li <zhigang.li@tendcloud.com>
2018-05-17 18:56:02 +0800

    为hasOneOf方法添加注释

--- src/libs/tools.js
+++ src/libs/tools.js
@@ -28,6 +28,11 @@ export const getIntersection = (arr1, arr2) => {
   return Array.from(new Set([...arr1, ...arr2]))
 }
 
+/**
+ * @param {Array} target 目标数组
+ * @param {Array} arr 需要查询的数组
+ * @description 判断要查询的数组是否至少有一个元素包含在目标数组中
+ */
 export const hasOneOf = (target, arr) => {
   return target.some(_ => arr.indexOf(_) > -1)
 }
