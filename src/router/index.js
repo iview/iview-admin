f280891e8b92eb0e68601fd263eac6a3cbebd424
zhigang.li <zhigang.li@tendcloud.com>
2018-05-17 10:58:52 +0800

    增加路由守卫基础，加入iview进度条

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import routes from './routers'
+import iView from 'iview'
 
 Vue.use(Router)
-
-export default new Router({
+const router = new Router({
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  iView.LoadingBar.start()
+  next()
+})
+
+router.afterEach(to => {
+  iView.LoadingBar.finish()
+  window.scrollTo(0, 0)
+})
+
+export default router
