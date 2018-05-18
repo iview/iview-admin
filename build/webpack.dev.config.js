06b01bb016d57d77b91a0625121e541b33014204
cuiweiqiang <cuiweiqiang.dlnu@gmail.com>
2018-05-18 10:20:09 +0800

    fix bug with dev.conf

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const package = require('../package.json');
 
 fs.open('./build/env.js', 'w', function(err, fd) {
-    const buf = 'export default "development";';
+    const buf = Buffer.from('export default "development";');
     fs.write(fd, buf, 0, buf.length, 0, function(err, written, buffer) {});
 });
 
