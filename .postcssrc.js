06fdbf6da4e55665fada2027d6c32f802c63b5dd
zhigang.li <zhigang.li@tendcloud.com>
2018-05-18 11:55:17 +0800

    使用vue-cli3.0

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,10 +1,5 @@
-// https://github.com/michael-ciniawsky/postcss-load-config
-
 module.exports = {
-  "plugins": {
-    "postcss-import": {},
-    "postcss-url": {},
-    // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+  plugins: {
+    autoprefixer: {}
   }
 }
---
+++ tests/unit/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    mocha: true
+  },
+  rules: {
+    'import/no-extraneous-dependencies': 'off'
+  }
+}
