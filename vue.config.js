156be2b90cc42fa3a3dae82251ceb0f418a4b394
zhigang.li <zhigang.li@tendcloud.com>
2018-05-18 18:23:23 +0800

    更新vue.config.js的baseurl

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,10 @@ const env = process.env.NODE_ENV
 fs.writeFileSync(path.join(__dirname, './config/env.js'), `export default '${env}'
 `)
 
+const BASE_URL = 'https://iview.github.io/iview-admin/'
+
 module.exports = {
+  baseUrl: BASE_URL,
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
