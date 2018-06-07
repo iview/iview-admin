97f774fe6c8dd83efc599e05711fd44b8815c4ae
zhigang.li <zhigang.li@tendcloud.com>
2018-06-07 16:52:41 +0800

    面包屑图标右侧加4px间距

--- src/view/main/components/header-bar/custom-bread-crumb/custom-bread-crumb.vue
+++ src/view/main/components/header-bar/custom-bread-crumb/custom-bread-crumb.vue
@@ -2,7 +2,7 @@
   <div class="custom-bread-crumb">
     <Breadcrumb :style="{fontSize: `${fontSize}px`}">
       <BreadcrumbItem v-for="item in list" :to="item.to" :key="`bread-crumb-${item.name}`">
-        <common-icon :type="item.icon || ''"/>
+        <common-icon style="margin-right: 4px;" :type="item.icon || ''"/>
         {{ showTitle(item) }}
       </BreadcrumbItem>
     </Breadcrumb>
