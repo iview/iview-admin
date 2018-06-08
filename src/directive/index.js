5850519420d3d29ab9ae08a156b233939eeeee86
zhigang.li <zhigang.li@tendcloud.com>
2018-06-08 15:57:08 +0800

    update

--- src/directive/index.js
+++ src/directive/index.js
@@ -5,7 +5,8 @@ const importDirective = Vue => {
    * 拖拽指令 v-draggable="options"
    * options = {
    *  trigger: /这里传入作为拖拽触发器的CSS选择器/,
-   *  body:    /这里传入需要移动容器的CSS选择器/
+   *  body:    /这里传入需要移动容器的CSS选择器/,
+   *  recover: /拖动结束之后是否恢复到原来的位置/
    * }
    */
   Vue.directive('draggable', directive.draggable)
