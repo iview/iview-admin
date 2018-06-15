e881bc0df100eec98d5fa5e380e444c9dce0b682
zhigang.li <zhigang.li@tendcloud.com>
2018-06-15 12:07:29 +0800

    commit

---
+++ src/components/img-cropper/img-cropper.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="cropper-wrapper">
+    <img ref="cropperImg" :src="src" alt="">
+  </div>
+</template>
+
+<script>
+import Cropper from 'cropperjs/dist/cropper.esm.js'
+import 'cropperjs/dist/cropper.min.css'
+export default {
+  name: 'ImgCropper',
+  props: {
+    src: {
+      type: String,
+      default: ''
+    },
+    dragMode: {
+      type: String,
+      default: 'move'
+    },
+    previewId: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      cropper: null
+    }
+  },
+  mounted () {
+    this.cropper = new Cropper(this.$refs.cropperImg, {
+      aspectRatio: 16 / 9,
+      dragMode: this.dragMode,
+      preview: this.previewId,
+      restore: false,
+      center: false,
+      highlight: false,
+      cropBoxMovable: false,
+      toggleDragModeOnDblclick: false
+    })
+  }
+}
+</script>
+
+<style lang="less">
+@import './index.less';
+</style>
--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -5,5 +5,5 @@ module.exports = (on, config) => Object.assign({}, config, {
   integrationFolder: 'tests/e2e/specs',
   screenshotsFolder: 'tests/e2e/screenshots',
   videosFolder: 'tests/e2e/videos',
-  supportFile: 'tests/e2e/support/index.js',
-});
+  supportFile: 'tests/e2e/support/index.js'
+})
