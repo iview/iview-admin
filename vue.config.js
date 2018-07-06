cfcd15a0ae82e1d2b36c955c4456dab34c34411e
zhigang.li <zhigang.li@tendcloud.com>
2018-07-06 16:45:14 +0800

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -33,5 +33,7 @@ module.exports = {
       .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
       .set('_c', resolve('src/components'))
       .set('_conf', resolve('config'))
-  }
+  },
+  // 打包时不生成.map文件
+  productionSourceMap: false
 }
