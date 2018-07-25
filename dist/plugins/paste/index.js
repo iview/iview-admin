b50a6750911661d31a5209db3e471074bab64d25
zhigang.li <zhigang.li@tendcloud.com>
2018-07-25 18:16:02 +0800

    remove

--- dist/plugins/paste/index.js
+++
@@ -1,7 +0,0 @@
-// Exports the "paste" plugin for usage with module loaders
-// Usage:
-//   CommonJS:
-//     require('tinymce/plugins/paste')
-//   ES2015:
-//     import 'tinymce/plugins/paste'
-require('./plugin.js');
--- dist/plugins/toc/index.js
+++
@@ -1,7 +0,0 @@
-// Exports the "toc" plugin for usage with module loaders
-// Usage:
-//   CommonJS:
-//     require('tinymce/plugins/toc')
-//   ES2015:
-//     import 'tinymce/plugins/toc'
-require('./plugin.js');
