da38bdf35196f0ba7b80c1dbf55ba7e03b16b775
chengkang <1412950785@qq.com>
2018-08-02 11:54:49 +0800

    tables component允许设置列是否支持搜索以及自定义搜索方法 1. 有些时候我们需要进行后台的搜索, 而不只是简单的前台过滤 2. 有些时候对于某些列, 可能后台API不支持对其进行搜索

--- src/view/components/tables/tables.vue
+++ src/view/components/tables/tables.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Card>
-      <tables ref="tables" editable searchable search-place="top" v-model="tableData" :columns="columns" @on-delete="handleDelete"/>
+      <tables ref="tables" editable searchable search-place="top" v-model="tableData" :loading="loading" :columns="columns" @on-delete="handleDelete" @on-search="handleSearch"/>
       <Button style="margin: 10px 0;" type="primary" @click="exportExcel">导出为Csv文件</Button>
     </Card>
   </div>
@@ -17,10 +17,11 @@ export default {
   },
   data () {
     return {
+      loading: false,
       columns: [
-        {title: 'Name', key: 'name', sortable: true},
-        {title: 'Email', key: 'email', editable: true},
-        {title: 'Create-Time', key: 'createTime'},
+        {title: 'Name', key: 'name', sortable: true, searchable: true},
+        {title: 'Email', key: 'email', editable: true, searchable: true},
+        {title: 'Create-Time', key: 'createTime', searchable: true},
         {
           title: 'Handle',
           key: 'handle',
@@ -51,6 +52,13 @@ export default {
   methods: {
     handleDelete (params) {
       console.log(params)
+    },
+    handleSearch (params) {
+      // 在这里模拟进行远程过滤
+      this.loading = true
+      setTimeout(() => {
+        this.loading = false
+      }, 1000)
     },
     exportExcel () {
       this.$refs.tables.exportCsv({
