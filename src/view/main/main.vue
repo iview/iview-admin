34102a6e372eec8e956f0b78a381cd15b769da86
zhigang.li <zhigang.li@tendcloud.com>
2018-08-08 14:55:06 +0800

    修复左侧菜单超出后没有滚动条的bug

--- src/view/main/main.vue
+++ src/view/main/main.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout style="height: 100%" class="main">
-    <Sider hide-trigger collapsible :width="256" :collapsed-width="64" v-model="collapsed">
-      <side-menu accordion :active-name="$route.name" :collapsed="collapsed" @on-select="turnToPage" :menu-list="menuList">
+    <Sider hide-trigger collapsible :width="256" :collapsed-width="64" v-model="collapsed" class="left-sider" style="overflow: hidden;">
+      <side-menu  :active-name="$route.name" :collapsed="collapsed" @on-select="turnToPage" :menu-list="menuList">
         <!-- 需要放在菜单上面的内容，如Logo，写在side-menu标签内部，如下 -->
         <div class="logo-con">
           <img v-show="!collapsed" :src="maxLogo" key="max-logo" />
