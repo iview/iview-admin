f04bbb940f5dedc9318a3b87d3c2234f33c08ab7
zhigang.li <zhigang.li@tendcloud.com>
2018-08-17 16:02:07 +0800

    update router.js

--- src/router/routers.js
+++ src/router/routers.js
@@ -163,6 +163,35 @@ export default [
     ]
   },
   {
+    path: '/excel',
+    name: 'excel',
+    meta: {
+      icon: 'ios-stats',
+      title: 'EXCEL导入导出'
+    },
+    component: Main,
+    children: [
+      {
+        path: 'upload-excel',
+        name: 'upload-excel',
+        meta: {
+          icon: 'md-add',
+          title: '导入EXCEL'
+        },
+        component: () => import('@/view/excel/upload-excel.vue')
+      },
+      {
+        path: 'export-excel',
+        name: 'export-excel',
+        meta: {
+          icon: 'md-download',
+          title: '导出EXCEL'
+        },
+        component: () => import('@/view/excel/export-excel.vue')
+      }
+    ]
+  },
+  {
     path: '/tools_methods',
     name: 'tools_methods',
     meta: {
