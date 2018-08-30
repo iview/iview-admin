7c58c94a42dac179d40384e775ea2ae453e4ba47
zhigang.li <zhigang.li@tendcloud.com>
2018-08-30 18:15:42 +0800

    添加设置代理解决跨域示例

--- vue.config.js
+++ vue.config.js
@@ -34,4 +34,8 @@ module.exports = {
   },
   // 打包时不生成.map文件
   productionSourceMap: false
+  // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
+  // devServer: {
+  //   proxy: 'localhost:3000'
+  // }
 }
