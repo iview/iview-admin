5eb4bd957f908752d49843bd0dd5aba1dd3b7f14
XiaoBingBy <XiaoBingBy@Gmail.com>
2018-09-10 01:13:51 +0800

    update user

--- src/view/sys/user/user.vue
+++ src/view/sys/user/user.vue
@@ -1,25 +1,36 @@
 <template>
   <Card>
-    <div>
-      <Form ref="formInline" inline>
-        <Row>
-          <Col span="8">
-            <FormItem label="开始时间-结束时间:" :label-width="129">
-              <DatePicker type="datetimerange" placement="bottom-end" format="yyyy-MM-dd HH:mm:ss" placeholder="搜索时间"  style="min-width: 280px;">
-              </DatePicker>
-            </FormItem>
-          </Col>
-          <Col>
-            <!--<FormItem  style="position: absolute;right: 0;">-->
-              <!--<Button class="search-btn" icon="ios-refresh-circle-outline" type="primary">重置</Button>-->
-              <!--<Button class="search-btn" icon="md-search" type="primary">搜索</Button>-->
-            <!--</FormItem>-->
-          </Col>
-        </Row>
-      </Form>
-    </div>
-    <!--<Table border stripe :loading="loading" :columns="columns" :data="data"></Table>-->
-    <Table border stripe></Table>
+      <Row>
+        <Form ref="formInline" inline :label-width="70">
+          <Form-item label="用户名称:">
+            <Input type="text" clearable placeholder="请输入用户名" style="width: 200px"/>
+          </Form-item>
+          <Form-item label="用户名称:">
+            <Input type="text" clearable placeholder="请输入用户名" style="width: 200px"/>
+          </Form-item>
+          <Form-item style="margin-left:-35px;" class="br">
+            <div class="opt-group">
+              <Button class="opt-btn" type="primary" icon="ios-search">搜索</Button>
+              <Button class="opt-btn">重置</Button>
+            </div>
+          </Form-item>
+        </Form>
+      </Row>
+      <Row class="operation" type="flex" justify="space-between">
+        <div class="opt-group">
+          <Button class="opt-btn" type="primary" icon="md-add">添加</Button>
+          <Button class="opt-btn" icon="md-trash">删除</Button>
+        </div>
+        <div>
+          <Button icon="md-refresh">刷新</Button>
+        </div>
+      </Row>
+      <Row>
+        <Table border stripe></Table>
+      </Row>
+      <Row type="flex" justify="center" class="page">
+        <Page :total="100" show-sizer />
+      </Row>
   </Card>
 </template>
 
@@ -29,6 +40,23 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+  .operation {
+    margin-bottom: 2vh;
+  }
+  .opt-group {
+    .opt-btn:first-child {
+      margin-left: 0;
+    }
+    .opt-btn:last-child {
+      margin-right: 0;
+    }
+    .opt-btn {
+      margin-left: 2px;
+      margin-right: 2px;
+    }
+  }
+  .page {
+    margin-top: 2vh;
+  }
 </style>
