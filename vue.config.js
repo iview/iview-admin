d74624c94c12efd8c83d725b90e4742e6fced95e
zhigang.li@tendcloud.com <zhigang.li@tendcloud.com>
2018-09-27 10:43:31 +0800

    添加关闭eslint配置

--- vue.config.js
+++ vue.config.js
@@ -26,6 +26,8 @@ module.exports = {
   baseUrl: BASE_URL,
   // tweak internal webpack configuration.
   // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+  // 如果你不需要使用eslint，把lintOnSave设为false即可
+  lintOnSave: true,
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
