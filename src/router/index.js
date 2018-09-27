222242dccca1c6fd691991a1ac5f5c80707f5109
zhigang.li@tendcloud.com <zhigang.li@tendcloud.com>
2018-09-27 18:19:10 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,11 @@ const router = new Router({
 })
 const LOGIN_PAGE_NAME = 'login'
 
+const turnTo = (to, access, next) => {
+  if (canTurnTo(to.name, access, routes)) next() // 有权限，可访问
+  else next({ replace: true, name: 'error_401' }) // 无权限，重定向到401页面
+}
+
 router.beforeEach((to, from, next) => {
   iView.LoadingBar.start()
   const token = getToken()
@@ -29,11 +34,18 @@ router.beforeEach((to, from, next) => {
       name: 'home' // 跳转到home页
     })
   } else {
-    store.dispatch('getUserInfo').then(user => {
-      // 拉取用户信息，通过用户权限和跳转的页面的name来判断是否有权限访问;access必须是一个数组，如：['super_admin'] ['super_admin', 'admin']
-      if (canTurnTo(to.name, user.access, routes)) next() // 有权限，可访问
-      else next({ replace: true, name: 'error_401' }) // 无权限，重定向到401页面
-    })
+    if (store.state.user.hasGetInfo) {
+      turnTo(to, store.state.user.access, next)
+    } else {
+      store.dispatch('getUserInfo').then(user => {
+        // 拉取用户信息，通过用户权限和跳转的页面的name来判断是否有权限访问;access必须是一个数组，如：['super_admin'] ['super_admin', 'admin']
+        turnTo(to, user.access, next)
+      }).catch(() => {
+        next({
+          name: 'login'
+        })
+      })
+    }
   }
 })
 
