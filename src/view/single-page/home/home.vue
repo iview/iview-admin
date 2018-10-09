9b262e47fe46c14b43d3531b17b77cba524c6c3a
zhigang.li@tendcloud.com <zhigang.li@tendcloud.com>
2018-10-09 15:37:43 +0800

    update

--- src/view/single-page/home/home.vue
+++ src/view/single-page/home/home.vue
@@ -46,7 +46,7 @@ export default {
     return {
       inforCardData: [
         { title: '新增用户', icon: 'md-person-add', count: 803, color: '#2d8cf0' },
-        { title: '累计点击', icon: 'md-locate', count: 23432, color: '#19be6b' },
+        { title: '累计点击', icon: 'md-locate', count: 232, color: '#19be6b' },
         { title: '新增问答', icon: 'md-help-circle', count: 142, color: '#ff9900' },
         { title: '分享统计', icon: 'md-share', count: 657, color: '#ed3f14' },
         { title: '新增互动', icon: 'md-chatbubbles', count: 12, color: '#E46CBB' },
