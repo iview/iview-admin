c45d437d3d4449f9e81650cec46e56bca34acf87
zhigang.li@tendcloud.com <zhigang.li@tendcloud.com>
2018-11-01 18:29:49 +0800

    补充路由配置注释

--- src/router/routers.js
+++ src/router/routers.js
@@ -4,7 +4,10 @@ import parentView from '@/components/parent-view'
 /**
  * iview-admin中meta除了原生参数外可配置的参数:
  * meta: {
- *  title: { String|Number|Function } 显示在侧边栏和
+ *  title: { String|Number|Function }
+ *         显示在侧边栏、面包屑和标签栏的文字
+ *         使用'{{ 多语言字段 }}'形式结合多语言使用，例子看多语言的路由配置;
+ *         可以传入一个回调函数，参数是当前路由对象，例子看动态路由和带参路由
  *  hideInMenu: (false) 设为true后在左侧菜单不会显示该页面选项
  *  notCache: (false) 设为true后页面不会缓存
  *  access: (null) 可访问该页面的权限数组，当前路由设置的权限会影响子路由
