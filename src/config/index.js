46ce4cf26cd34cbb59ba989ed135d4ff6ff8b557
zhigang.li@tendcloud.com <zhigang.li@tendcloud.com>
2018-11-01 18:42:58 +0800

    恢复设置plugin['error-store'].developmentOff设置为false

--- src/config/index.js
+++ src/config/index.js
@@ -26,7 +26,7 @@ export default {
   plugin: {
     'error-store': {
       showInHeader: true, // 设为false后不会在顶部显示错误日志徽标
-      developmentOff: true // 设为true后在开发环境不会收集错误信息，方便开发中排查错误
+      developmentOff: false // 设为true后在开发环境不会收集错误信息，方便开发中排查错误
     }
   }
 }
