454ab6fec6737f931f17360b2d221d578c145351
zhigang.li@tendcloud.com <zhigang.li@tendcloud.com>
2018-11-02 11:16:52 +0800

    commit

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ const resolve = dir => {
 // 例如：https://www.foobar.com/my-app/
 // 需要将它改为'/my-app/'
 const BASE_URL = process.env.NODE_ENV === 'production'
-  ? '/iview-admin/'
+  ? '/'
   : '/'
 
 module.exports = {
