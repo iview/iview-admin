1da243719c4da6f0a35b95c5b8542351089eb820
zhigang.li@tendcloud.com <zhigang.li@tendcloud.com>
2018-11-02 10:26:50 +0800

    打包时不生成.map文件

--- vue.config.js
+++ vue.config.js
@@ -34,7 +34,7 @@ module.exports = {
       .set('_c', resolve('src/components'))
   },
   // 打包时不生成.map文件
-  productionSourceMap: true
+  productionSourceMap: false
   // 这里写你调用接口的基础路径，来解决跨域，如果设置了代理，那你本地开发环境的axios的baseUrl要写为 '' ，即空字符串
   // devServer: {
   //   proxy: 'localhost:3000'
