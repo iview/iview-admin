daad063c4f1ac87bd66a390df71a11ceb6c39f58
Leon WuV <wuxiaolong802@163.com>
2018-12-27 13:25:43 +0800

    :pencil2: 别名配置@为cli3自带功能
    别名配置@为cli3自带功能，没必要在配置文件里配置

--- vue.config.js
+++ vue.config.js
@@ -31,7 +31,7 @@ module.exports = {
   lintOnSave: true,
   chainWebpack: config => {
     config.resolve.alias
-      .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
+      //.set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
       .set('_c', resolve('src/components'))
   },
   // 设为false打包时不生成.map文件
