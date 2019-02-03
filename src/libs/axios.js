eca5bc476c12759ff3652ecf3d7db3dd87bd6f72
XiaoBingBy <XiaoBingBy@Gmail.com>
2019-02-04 00:15:10 +0800

    [修改][token]

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import store from '@/store'
+import { getToken } from '@/libs/util'
 // import { Spin } from 'iview'
 const addErrorLog = errorInfo => {
   const { statusText, status, request: { responseURL } } = errorInfo
@@ -22,6 +23,7 @@ class HttpRequest {
       baseURL: this.baseUrl,
       headers: {
         //
+        'Authorization': getToken() ? `Bearer ${getToken()}` : ''
       }
     }
     return config
