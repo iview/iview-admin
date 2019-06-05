86861822574f9fac255d2ba6f41d66820b7b8717
Vincent Guo <dreambo8563@outlook.com>
2019-06-05 09:49:05 +0800

    rename variable name to error500

--- src/view/error-page/500.vue
+++ src/view/error-page/500.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import error404 from '@/assets/images/error-page/error-500.svg'
+import error500 from '@/assets/images/error-page/error-500.svg'
 import errorContent from './error-content.vue'
 export default {
   name: 'error_500',
@@ -12,7 +12,7 @@ export default {
   },
   data () {
     return {
-      src: error404
+      src: error500
     }
   }
 }
