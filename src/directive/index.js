3396914d6582ce9a06c29b464b7a16b8c30d1977
RyomaHan <ryomahan1996@gmail.com>
2020-03-06 12:37:50 +0800

    Update index.js
    update a notes bug

--- src/directive/index.js
+++ src/directive/index.js
@@ -11,7 +11,7 @@ const importDirective = Vue => {
    */
   Vue.directive('draggable', directive.draggable)
   /**
-   * clipboard指令 v-draggable="options"
+   * clipboard指令 v-clipboard="options"
    * options = {
    *  value:    /在输入框中使用v-model绑定的值/,
    *  success:  /复制成功后的回调/,
