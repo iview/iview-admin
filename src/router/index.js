bf9bcec610399c6cae3e707849b0d432457075c9
simon9124 <435776313@qq.com>
2020-04-27 15:58:57 +0800

    部署到github gh-pages -> 取消路由history模式

--- src/router/index.js
+++ src/router/index.js
@@ -1,60 +1,61 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import routes from './routers'
-import store from '@/store'
-import iView from 'iview'
-import { setToken, getToken, canTurnTo } from '@/libs/util'
-import config from '@/config'
-const { homeName } = config
+import Vue from "vue";
+import Router from "vue-router";
+import routes from "./routers";
+import store from "@/store";
+import iView from "iview";
+import { setToken, getToken, canTurnTo } from "@/libs/util";
+import config from "@/config";
+const { homeName } = config;
 
-Vue.use(Router)
+Vue.use(Router);
 const router = new Router({
-  routes,
-  mode: 'history'
-})
-const LOGIN_PAGE_NAME = 'login'
+  routes
+  // 部署到github gh-pages -> 取消history
+  // mode: "history"
+});
+const LOGIN_PAGE_NAME = "login";
 
 const turnTo = (to, access, next) => {
-  if (canTurnTo(to.name, access, routes)) next() // 有权限，可访问
-  else next({ replace: true, name: 'error_401' }) // 无权限，重定向到401页面
-}
+  if (canTurnTo(to.name, access, routes)) next(); // 有权限，可访问
+  else next({ replace: true, name: "error_401" }); // 无权限，重定向到401页面
+};
 
 router.beforeEach((to, from, next) => {
-  iView.LoadingBar.start()
-  const token = getToken()
+  iView.LoadingBar.start();
+  const token = getToken();
   if (!token && to.name !== LOGIN_PAGE_NAME) {
     // 未登录且要跳转的页面不是登录页
     next({
       name: LOGIN_PAGE_NAME // 跳转到登录页
-    })
+    });
   } else if (!token && to.name === LOGIN_PAGE_NAME) {
     // 未登陆且要跳转的页面是登录页
-    next() // 跳转
+    next(); // 跳转
   } else if (token && to.name === LOGIN_PAGE_NAME) {
     // 已登录且要跳转的页面是登录页
     next({
       name: homeName // 跳转到homeName页
-    })
+    });
   } else {
     if (store.state.user.hasGetInfo) {
-      turnTo(to, store.state.user.access, next)
+      turnTo(to, store.state.user.access, next);
     } else {
-      store.dispatch('getUserInfo').then(user => {
+      store.dispatch("getUserInfo").then(user => {
         // 拉取用户信息，通过用户权限和跳转的页面的name来判断是否有权限访问;access必须是一个数组，如：['super_admin'] ['super_admin', 'admin']
-        turnTo(to, user.access, next)
+        turnTo(to, user.access, next);
       }).catch(() => {
-        setToken('')
+        setToken("");
         next({
-          name: 'login'
-        })
-      })
+          name: "login"
+        });
+      });
     }
   }
-})
+});
 
 router.afterEach(to => {
-  iView.LoadingBar.finish()
-  window.scrollTo(0, 0)
-})
+  iView.LoadingBar.finish();
+  window.scrollTo(0, 0);
+});
 
-export default router
+export default router;
