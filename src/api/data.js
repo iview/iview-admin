5bdccf38440af2387a88f0ddb0ef3f9f7c20eb4e
simon9124 <435776313@qq.com>
2020-04-27 18:17:44 +0800

    注册mock事件：getAllMenus

--- src/api/data.js
+++ src/api/data.js
@@ -1,30 +1,40 @@
-import axios from '@/libs/api.request'
+import axios from "@/libs/api.request";
 
 export const getTableData = () => {
   return axios.request({
-    url: 'get_table_data',
-    method: 'get'
-  })
-}
+    url: "get_table_data",
+    method: "get"
+  });
+};
 
 export const getDragList = () => {
   return axios.request({
-    url: 'get_drag_list',
-    method: 'get'
-  })
-}
+    url: "get_drag_list",
+    method: "get"
+  });
+};
 
 export const errorReq = () => {
   return axios.request({
-    url: 'error_url',
-    method: 'post'
-  })
-}
+    url: "error_url",
+    method: "post"
+  });
+};
 
 export const saveErrorLogger = info => {
   return axios.request({
-    url: 'save_error_logger',
+    url: "save_error_logger",
     data: info,
-    method: 'post'
-  })
-}
+    method: "post"
+  });
+};
+
+// 获取菜单数据
+export const getAllMenus = token => {
+  if (token !== undefined) {
+    return axios.request({
+      url: "get_all_menus",
+      method: "get"
+    });
+  }
+};
--- src/mock/data.js
+++ src/mock/data.js
@@ -1,26 +1,36 @@
-import Mock from 'mockjs'
-import { doCustomTimes } from '@/libs/util'
-const Random = Mock.Random
+import Mock from "mockjs";
+import { doCustomTimes } from "@/libs/util";
+import { menuList } from "./role"; // 菜单列表
+const Random = Mock.Random;
 
 export const getTableData = req => {
-  let tableData = []
+  let tableData = [];
   doCustomTimes(5, () => {
-    tableData.push(Mock.mock({
-      name: '@name',
-      email: '@email',
-      createTime: '@date'
-    }))
-  })
-  return tableData
-}
+    tableData.push(
+      Mock.mock({
+        name: "@name",
+        email: "@email",
+        createTime: "@date"
+      })
+    );
+  });
+  return tableData;
+};
 
 export const getDragList = req => {
-  let dragList = []
+  let dragList = [];
   doCustomTimes(5, () => {
-    dragList.push(Mock.mock({
-      name: Random.csentence(10, 13),
-      id: Random.increment(10)
-    }))
-  })
-  return dragList
-}
+    dragList.push(
+      Mock.mock({
+        name: Random.csentence(10, 13),
+        id: Random.increment(10)
+      })
+    );
+  });
+  return dragList;
+};
+
+// 获取菜单列表
+export const getAllMenus = req => {
+  return { status: 200, message: "成功！", data: menuList };
+};
