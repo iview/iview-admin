fc9601db497fa2ddc98d50125010bc00eb324be1
simon9124 <435776313@qq.com>
2020-04-27 14:03:32 +0800

    登录事件改造，获取token才拉取用户信息

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -1,11 +1,13 @@
 <style lang="less">
-  @import './login.less';
+@import "./login.less";
 </style>
 
 <template>
   <div class="login">
     <div class="login-con">
-      <Card icon="log-in" title="欢迎登录" :bordered="false">
+      <Card icon="log-in"
+            title="欢迎登录"
+            :bordered="false">
         <div class="form-con">
           <login-form @on-success-valid="handleSubmit"></login-form>
           <p class="login-tip">输入任意用户名和密码即可</p>
@@ -16,30 +18,27 @@
 </template>
 
 <script>
-import LoginForm from '_c/login-form'
-import { mapActions } from 'vuex'
+import LoginForm from "_c/login-form";
+import { mapActions } from "vuex";
 export default {
   components: {
     LoginForm
   },
   methods: {
-    ...mapActions([
-      'handleLogin',
-      'getUserInfo'
-    ]),
-    handleSubmit ({ userName, password }) {
+    ...mapActions(["handleLogin", "getUserInfo"]),
+    handleSubmit({ userName, password }) {
       this.handleLogin({ userName, password }).then(res => {
-        this.getUserInfo().then(res => {
-          this.$router.push({
-            name: this.$config.homeName
-          })
-        })
-      })
+        res.data.status === 200 &&
+          this.getUserInfo().then(res => {
+            this.$router.push({
+              name: this.$config.homeName
+            });
+          });
+      });
     }
   }
-}
+};
 </script>
 
 <style>
-
 </style>
