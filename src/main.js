79cab7ef188b4869985f3b08b255a4e8d9efe617
simon9124 <435776313@qq.com>
2020-04-27 15:51:26 +0800

    github部署时，生产环境同样引入mock

--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import store from './store'
-import iView from 'iview'
-import i18n from '@/locale'
-import config from '@/config'
-import importDirective from '@/directive'
-import installPlugin from '@/plugin'
-import 'iview/dist/styles/iview.css'
-import './index.less'
-import '@/assets/icons/iconfont.css'
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import store from "./store";
+import iView from "iview";
+import i18n from "@/locale";
+import config from "@/config";
+import importDirective from "@/directive";
+import installPlugin from "@/plugin";
+import "iview/dist/styles/iview.css";
+import "./index.less";
+import "@/assets/icons/iconfont.css";
 // 实际打包时应该不引入mock
 /* eslint-disable */
-if (process.env.NODE_ENV !== 'production') require('@/mock')
+// if (process.env.NODE_ENV !== 'production')
+require('@/mock')
 
 Vue.use(iView, {
   i18n: (key, value) => i18n.t(key, value)
