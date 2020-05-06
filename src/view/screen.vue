cdafa1ffbf0214e8e4c599d859c3d9c1a28642b4
simon9124 <435776313@qq.com>
2020-05-06 14:09:10 +0800

    大屏界面

---
+++ src/view/screen.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    {{title}}
+    <Button size="large"
+            type="text"
+            @click="backPrev">返回上一页</Button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: ""
+    };
+  },
+  created() {
+    this.title = this.$route.meta.title;
+  },
+  methods: {
+    backPrev() {
+      this.$router.go(-1);
+    }
+  },
+  watch: {
+    $route(to, from) {
+      this.title = to.meta.title;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
