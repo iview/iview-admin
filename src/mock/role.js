18800a4bfb9cf90662658dd798f3461c97dd3a22
simon9124 <435776313@qq.com>
2020-05-06 14:09:57 +0800

    mockData：角色更新

--- src/mock/role.js
+++ src/mock/role.js
@@ -3,248 +3,193 @@ const roleList = [
   {
     name: "super_admin",
     title: "超级管理员",
-    menus: [],
+    menus: [
+      "doc",
+      "control",
+      "multilevel",
+      "level_2_1",
+      "level_2_2",
+      "level_2_2_1",
+      "screen2",
+      "level_2_2_2",
+      "level_2_3",
+      "screen",
+      "github"
+    ],
     id: "1"
   },
   {
     name: "visitor",
     title: "访客",
-    menus: [],
+    menus: [
+      "control",
+      "multilevel",
+      "level_2_1",
+      "level_2_2",
+      "level_2_2_1",
+      "level_2_2_2",
+      "level_2_3",
+      "screen"
+    ],
     id: "2"
   }
 ];
 
-// 菜单列表
-const menuList = [
+/** menuList路由列表
+ * isOutSide：是否为外链
+ * showLevel：路由层级，详见menuLevel路由层级列表
+ * path：前端组件路径（父组件为空）
+ */
+
+// 路由层级
+const menuLevel = [
   {
-    id: "a4a49ff8874044dfbd90c06544be165b",
-    name: "home",
-    title: "首页概览",
-    url: "/home",
-    path: "single-page/home/home",
-    sort: 30,
-    parenetId: "root",
-    parenetPath: "root",
-    status: 1,
-    description: "",
-    ico: "md-home",
-    isOutSide: false,
-    showLevel: 3
+    label: "菜单显示该页面选项，页面不含菜单栏",
+    value: "1"
   },
   {
-    id: "3976b690e56b48639e108ac6d6ca622b",
-    name: "control-leader-shop",
+    label: "菜单显示该页面选项，页面含菜单栏",
+    value: "2"
+  },
+  {
+    label: "菜单隐藏该页面选项，页面不含菜单栏",
+    value: "3"
+  }
+];
+
+// 路由列表
+const menuList = [
+  {
+    id: "doc",
+    name: "doc",
+    title: "文档",
+    url: "https://lison16.github.io/iview-admin-doc/#/",
+    path: "",
+    sort: 28,
+    parentId: "root",
+    ico: "ios-book",
+    isOutSide: true,
+    showLevel: 1
+  },
+  {
+    id: "control",
+    name: "control",
     title: "驾驶舱",
-    url: "control-leader-shop",
-    path: "6inspector/control",
-    sort: 25,
-    parenetId: "root",
-    parenetPath: "root",
-    status: 1,
-    description: "驾驶舱-车间（管理员或车间主管）",
+    url: "control",
+    path: "screen",
+    sort: 26,
+    parentId: "root",
     ico: "md-laptop",
     isOutSide: false,
     showLevel: 1
   },
   {
-    id: "ef93f8fe1b154de688bcbaab1a2a88ef",
-    name: "task",
-    title: "任务管理",
-    url: "task",
+    id: "multilevel",
+    name: "multilevel",
+    title: "多级菜单",
+    url: "multilevel",
     path: "",
-    sort: 15,
-    parenetId: "root",
-    parenetPath: "root",
-    status: 1,
-    description: "",
-    ico: "md-shuffle",
+    sort: 24,
+    parentId: "root",
+    ico: "md-menu",
     isOutSide: false,
-    showLevel: 3
+    showLevel: 2
   },
   {
-    id: "8502de35df2c4d19ad337c5548f661b4",
-    name: "sopMessage/message",
-    title: "查看SOP",
-    url: "sopMessage/message",
-    path: "8taskManage/sopMessage",
-    sort: 13,
-    parenetId: "root",
-    parenetPath: "root",
-    status: 1,
-    description: "",
-    ico: "md-document",
+    id: "level_2_1",
+    name: "level_2_1",
+    title: "二级-1",
+    url: "level_2_1",
+    path: "template",
+    sort: 10,
+    parentId: "multilevel",
+    ico: "md-funnel",
     isOutSide: false,
-    showLevel: 3
+    showLevel: 2
   },
   {
-    id: "2",
-    name: "account",
-    title: "账号管理",
-    url: "account",
-    path: "3manage/account",
-    sort: 13,
-    parenetId: "d30bf0ff035c4e51875e7014ad46e1af",
-    parenetPath: "root.d30bf0ff035c4e51875e7014ad46e1af",
-    status: 1,
-    description: "系统账号管理",
-    ico: null,
+    id: "level_2_2",
+    name: "level_2_2",
+    title: "二级-2",
+    url: "level_2_2",
+    path: "parentView",
+    sort: 8,
+    parentId: "multilevel",
+    ico: "md-funnel",
     isOutSide: false,
-    showLevel: 3
+    showLevel: 2
   },
   {
-    id: "3",
-    name: "role",
-    title: "角色管理",
-    url: "role",
-    path: "3manage/role",
-    sort: 12,
-    parenetId: "d30bf0ff035c4e51875e7014ad46e1af",
-    parenetPath: "root.d30bf0ff035c4e51875e7014ad46e1af",
-    status: 1,
-    description: "系统角色管理",
-    ico: null,
+    id: "level_2_2_1",
+    name: "level_2_2_1",
+    title: "三级-1",
+    url: "level_2_2_1",
+    path: "template",
+    sort: 10,
+    parentId: "level_2_2",
+    ico: "md-funnel",
     isOutSide: false,
-    showLevel: 3
+    showLevel: 2
   },
   {
-    id: "23001d1a12fe43ceac9769ee08f899f2",
-    name: "menu",
-    title: "菜单管理",
-    url: "menu",
-    path: "3manage/menu",
-    sort: 11,
-    parenetId: "d30bf0ff035c4e51875e7014ad46e1af",
-    parenetPath: "root.d30bf0ff035c4e51875e7014ad46e1af",
-    status: 1,
-    description: "",
-    ico: null,
+    id: "screen2",
+    name: "screen2",
+    title: "三级大屏",
+    url: "screen2",
+    path: "screen",
+    sort: 8,
+    parentId: "level_2_2",
+    ico: "md-laptop",
     isOutSide: false,
-    showLevel: 3
+    showLevel: 1
   },
   {
-    id: "c64f399d2c03445dbfa2b23c240e2dc6",
-    name: "number/print",
-    title: "条码打印",
-    url: "number/print",
-    path: "3manage/number",
-    sort: 11,
-    parenetId: "root",
-    parenetPath: "root",
-    status: 1,
-    description: "",
-    ico: "md-print",
+    id: "level_2_2_2",
+    name: "level_2_2_2",
+    title: "三级-2",
+    url: "level_2_2_2",
+    path: "template",
+    sort: 6,
+    parentId: "level_2_2",
+    ico: "md-funnel",
     isOutSide: false,
-    showLevel: 3
+    showLevel: 2
   },
   {
-    id: "013ad32e79c44427a8cad340fb056821",
-    name: "report",
-    title: "报表中心",
-    url: "report",
-    path: "",
-    sort: 3,
-    parenetId: "root",
-    parenetPath: "root",
-    status: 1,
-    description: "",
-    ico: "md-analytics",
+    id: "level_2_3",
+    name: "level_2_3",
+    title: "二级-3",
+    url: "level_2_3",
+    path: "template",
+    sort: 6,
+    parentId: "multilevel",
+    ico: "md-funnel",
     isOutSide: false,
-    showLevel: 3
+    showLevel: 2
   },
   {
-    id: "6b2d3c49373946989262e586d655dccf",
-    name: "mould",
-    title: "模板配置",
-    url: "mould",
-    path: "3manage/mould/index",
-    sort: 3,
-    parenetId: "013ad32e79c44427a8cad340fb056821",
-    parenetPath: "root.013ad32e79c44427a8cad340fb056821",
-    status: 1,
-    description: "",
-    ico: "",
+    id: "screen",
+    name: "screen",
+    title: "二级大屏",
+    url: "screen",
+    path: "screen",
+    sort: 4,
+    parentId: "multilevel",
+    ico: "md-laptop",
     isOutSide: false,
-    showLevel: 3
+    showLevel: 1
   },
   {
-    id: "3db9fdd0ada749dab01149fa0248eb23",
-    name: "distribute",
-    title: "任务派发",
-    url: "distribute",
-    path: "8taskManage/taskDistribution",
-    sort: 2,
-    parenetId: "ef93f8fe1b154de688bcbaab1a2a88ef",
-    parenetPath: "root.ef93f8fe1b154de688bcbaab1a2a88ef",
-    status: 1,
-    description: "",
-    ico: "",
-    isOutSide: false,
-    showLevel: 3
-  },
-  {
-    id: "d30bf0ff035c4e51875e7014ad46e1af",
-    name: "manage",
-    title: "管理中心",
-    url: "manage",
+    id: "github",
+    name: "github",
+    title: "github",
+    url: "https://github.com/simon9124/iview-dynamicRouter",
     path: "",
     sort: 2,
-    parenetId: "root",
-    parenetPath: "root",
-    status: 1,
-    description: "系统基础配置",
-    ico: "md-settings",
-    isOutSide: false,
-    showLevel: 3
-  },
-  {
-    id: "82814bef61934f5aadb4e1e84608f414",
-    name: "view",
-    title: "异常产品报表",
-    url: "view/12",
-    path: "3manage/mould/reportView",
-    sort: 1,
-    parenetId: "013ad32e79c44427a8cad340fb056821",
-    parenetPath: "root.013ad32e79c44427a8cad340fb056821",
-    status: 1,
-    description: "",
-    ico: "",
-    isOutSide: false,
-    showLevel: 3
-  },
-  {
-    id: "e7dfaf2ec6aa482c93410d9b4e2323e5",
-    name: "history",
-    title: "历史任务",
-    url: "history",
-    path: "8taskManage/taskHistory",
-    sort: 1,
-    parenetId: "ef93f8fe1b154de688bcbaab1a2a88ef",
-    parenetPath: "root.ef93f8fe1b154de688bcbaab1a2a88ef",
-    status: 1,
-    description: "",
-    ico: "",
-    isOutSide: false,
-    showLevel: 3
-  }
-];
-
-// 菜单层级
-const menuLevel = [
-  {
-    label: "菜单显示该页面选项，页面不含菜单栏不含面包屑",
-    value: "1"
-  },
-  {
-    label: "菜单显示该页面选项，页面含菜单栏但不含面包屑",
-    value: "2"
-  },
-  {
-    label: "菜单显示该页面选项，页面含菜单栏和面包屑",
-    value: "3"
-  },
-  {
-    label: "菜单隐藏该页面选项，页面不含菜单栏不含面包屑",
-    value: "4"
+    parentId: "multilevel",
+    ico: "md-log-in",
+    isOutSide: true,
+    showLevel: 1
   }
 ];
 
