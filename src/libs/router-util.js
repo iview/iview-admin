007303ee4fcc748b1be20c999f1bae3a58508832
simon9124 <435776313@qq.com>
2020-06-01 16:11:27 +0800

    routerDataHanding通用方法更新：处理重定向

--- src/libs/router-util.js
+++ src/libs/router-util.js
@@ -208,7 +208,7 @@ export const routerDataHanding = apiRouterData => {
   };
   handleData(asyncRouterMap);
 
-  console.log(asyncRouterMap);
+  // console.log(asyncRouterMap);
 
   return asyncRouterMap;
 };
