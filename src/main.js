5ba93a829a23f2b57496049ab48ef823e4546655
simon9124 <435776313@qq.com>
2020-06-29 16:49:04 +0800

    全局注册事件中心

--- src/main.js
+++ src/main.js
@@ -15,33 +15,37 @@ import "@/assets/icons/iconfont.css";
 // 实际打包时应该不引入mock
 /* eslint-disable */
 // if (process.env.NODE_ENV !== 'production')
-require('@/mock')
+require("@/mock");
 
 Vue.use(iView, {
   i18n: (key, value) => i18n.t(key, value)
-})
+});
 /**
  * @description 注册admin内置插件
  */
-installPlugin(Vue)
+installPlugin(Vue);
 /**
  * @description 生产环境关掉提示
  */
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 /**
  * @description 全局注册应用配置
  */
-Vue.prototype.$config = config
+Vue.prototype.$config = config;
+/**
+ * @description 全局事件总线注册
+ */
+Vue.prototype.$EventBus = new Vue();
 /**
  * 注册指令
  */
-importDirective(Vue)
+importDirective(Vue);
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   i18n,
   store,
   render: h => h(App)
-})
+});
