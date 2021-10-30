a1cc03ec6bd29d4f08559bb967b7f4af53e3dfde
liwei <1078559858@qq.com>
2021-10-30 17:30:00 +0800

    修改逻辑

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
   // sub-path here. For example, if your app is deployed at
   // https://www.foobar.com/my-app/
   // then change this to '/my-app/'
-  publicPath: BASE_URL,
+  baseUrl: BASE_URL,
   // tweak internal webpack configuration.
   // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
   // 如果你不需要使用eslint，把lintOnSave设为false即可
